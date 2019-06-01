2fc3a2ef61a2061008c9f5361d82c0c83c8e6200
Sichen Wang <sichen.wang@qq.com>
2019-06-01 10:03:10 +1000

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by Jason on 1/6/19.
+//  Copyright © 2019 UTAS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
